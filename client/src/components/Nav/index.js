86a0cbd6242586c6d644754c64f9c46dd54f9920
Ulan Rakymzhanov <ulan@carta.healthcare>
2021-07-14 13:50:11 -0500

    more instructions

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
       <a className="navbar-brand" href="/">
-        My Reading List
+        ReadABit
       </a>
     </nav>
   );
